e1247cae1713850a8a15c7bc816cfc8fee2b18de
emanuelvogel19 <emanuelvogel19@gmail.com>
2025-08-09 20:34:27 -0300

    Centralizando TaskPage

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -8,7 +8,7 @@ function TaskPage(){
     const title = searchParams.get("title")
     const description = searchParams.get("description")
     return (
-        <div className="h-screen w-screen bg-slate-500 p-6">
+        <div className="h-screen w-screen bg-slate-500 p-6 flex justify-center">
             <div className="w-[500px] space-y-4">
                 <div className="flex justify-center relative mb-6 text-slate-100">
                     <button onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0">
